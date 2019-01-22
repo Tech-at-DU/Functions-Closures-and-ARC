6a44ce609837b75f03950a9e9e6137581b317cb3
Adriana <melina.gzz@hotmail.com>
2019-01-22 11:24:48 -0800

    Updated links.

--- content/Functions Closures and ARC.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ content/Functions Closures and ARC.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -1,24 +1,24 @@
 
 /*:
  # Closures
- 
+
  Closures, or nameless functions, are consided a first-class citizen in Swift. Meaning, a closure looks no different compared to a String in Swift.
- 
+
  With that said, you can use closures as a property of a class or a parameter in a function. You can also assign closures to variables to be used at a later point in time.
- 
+
  We'll be using our `Guest struct` from the previous tutorial. As a refresher, here's the struct:
  */
 struct Guest: CustomDebugStringConvertible, Equatable {
     var name: String
     var age: Int
-    
+
     var debugDescription: String {
         return name
     }
 }
 /*:
  ## The Similarities a Closure has with a Function
- 
+
  Lets look at a function and see how it looks like if we wrote it as a closure:
  */
 
@@ -32,21 +32,21 @@ func printGuestInfo(guest: Guest) {
 printGuestInfo(guest: eric)
 
 /*:
- 
+
  Now for a closure, let first look at how a function is constructed:
- 
+
  ![function structure](./function-structure.png)
- 
+
  And here's a closure's structure:
- 
+
  ![closure structure](./closure-structure.png)
- 
+
  Although they're very similar, there are a few key differences:
  - The closure lacks the `func` keyword
  - The closure does not require a function name
  - The parameter list and return type are in front of the `{`
  - The return type and the body of the closure are separated by the `in` keyword
- 
+
  Now lets write a closure and assign it to a variable
  */
 
@@ -56,9 +56,9 @@ let blockOfCodeToPrintGuestInfo = { (guest: Guest) in
 
 /*:
  Any guesses on how we'd execute the closure?
- 
+
  If we were to hit play on this playground, the print statement inside the body of our closure is never executed, it's only contained inside the `blockOfCodeToPrintGuestInfo` variable.
- 
+
  1. try to execute the closure to print the guest info of sam
  */
 
@@ -82,28 +82,28 @@ blockOfCodeToPrintGuestInfo(sam)
  If you want to compare your answer, look here:
  ```
  let sam = Guest(name: "Sam", age: 17)
- 
+
  blockOfCodeToPrintGuestInfo(sam)
  ```
- 
+
  ## More Examples
  Here are some variables that contain different kind of closures:
  */
 
 //closure with no params and no return type
 var blockOfCode1: () -> () = {
-    
+
 }
 
 //closure with two params and no return type
 var blockOfCode2: (String, Int) -> () = { param1, param2 in
-    
+
     //param1 is a string
     print(param1)
-    
+
     //param2 is an int
     print(param2)
-    
+
 }
 
 //closure with two params and int as the return type
@@ -115,13 +115,13 @@ var blockOfCode3: (Int, Int) -> Int = { param1, param2 in
 let numbersOnlyValidator: (String) -> Bool = { (stringValue: String) in
     for aCharacter in stringValue {
         let aSingleString = String(aCharacter)
-        
+
         //explain why this checks if a string contains a non-digit string value
         if Int(aSingleString) == nil {
             return false
         }
     }
-    
+
     return true
 }
 
@@ -149,50 +149,6 @@ if numbersOnlyValidator(userInput) {
 } else {
     print("input from user was invalid")
 }
-
-/*:
- -------------------INSTRUCTOR NOTE-------------------
- 
- Below section is confusing, as specialRequest does not do anything. Contact student who wrote this section to understand their intent, or remove section.
- 
- -------------------INSTRUCTOR NOTE-------------------
- */
-
-/*:
- Closures are primarily used for two things:
- 1. **A container of code used as a utility**, like a validator, or a set a rules to sort by.
- 1. **A container of code to be executed later**, perhaps after a task is finished (like a network call is finished downloading a list of users from the internet).
- 
- ## Closures used as parameters of a function
- 
- Let's go back to our extravagent event we were previously planning. We need to easily be able to add guests to our guest list, but we also want to be aware of any special requests they may have (you know how Dory only eats those rare olives from Spain).
- 
- Let's write a function that adds a new guest to our list of guests, and also asks the guest if they have any special request they'd like us to execute.
- 
- This closure has a parameter of one integer. This integer represents the number of guests when the new guest was added.
- */
-
-var listOfGuests: [Guest] = []
-
-func add(newGuest: Guest, specialRequest: (Int) -> ()) {
-    
-    //add the new guest to the guest list
-    listOfGuests.append(newGuest)
-    
-    //get the count of the guest list
-    let numberOfGuests = listOfGuests.count
-    
-    //execute the special request and pass the number of guests into the closure
-    specialRequest(numberOfGuests)
-}
-
-/*:
- Now, use this new add guest function to add sara to the guest list. Her special request is a print statement that says "Turn up the music!"
- */
-
-let sara = Guest(name: "Sara", age: 23)
-
-//invite sara by using add(..) function here
 
 /*:
  We've now got a solid understanding of what closures are. Let's move on to the next section to learn more on how to use them:
@@ -223,4 +179,3 @@ let sara = Guest(name: "Sara", age: 23)
 
 
 import Foundation
-
--- content/Functions Closures and ARC.playground/Pages/Shorthand Closures.xcplaygroundpage/Contents.swift
+++ content/Functions Closures and ARC.playground/Pages/Shorthand Closures.xcplaygroundpage/Contents.swift
@@ -1,42 +1,34 @@
 /*:
  # Shorthand Closures
  So, closures can help us reduce code and store blocks of code to be executed later. But, the same closures we've been writing can be written using **Type Inferance.** Let's see how that look:
- 
+
  First let's set up our favorite `struct`:
  */
 struct Guest: CustomDebugStringConvertible, Equatable {
     var name: String
     var age: Int
-    
+
     var debugDescription: String {
         return name
     }
 }
 
 /*:
- -------------------INSTRUCTOR NOTE-------------------
- 
- Below section is related to the specialRequest section in Closures. Below is written as if the last section of Closures did not exist. If that changes, will need rewording below.
- 
- -------------------INSTRUCTOR NOTE-------------------
- */
-
-/*:
  Let's go back to our extravagent event we were previously planning. We need to easily be able to add guests to our guest list, but we also want to be aware of any special requests they may have (you know how Dory only eats those rare olives from Spain).
- 
+
  Let's write a function that adds a new guest to our list of guests, and also asks the guest if they have any special request they'd like us to execute. This closure has a parameter of one integer. This integer represents the number of guests when the new guest was added.
  */
 
 var listOfGuests: [Guest] = []
 
 func add(newGuest: Guest, specialRequest: (Int) -> ()) {
-    
+
     //add the new guest to the guest list
     listOfGuests.append(newGuest)
-    
+
     //get the count of the guest list
     let numberOfGuests = listOfGuests.count
-    
+
     //execute the special request and pass the number of guests into the closure
     specialRequest(numberOfGuests)
 }
@@ -48,7 +40,7 @@ func add(newGuest: Guest, specialRequest: (Int) -> ()) {
 /*:
  ## Trailing Closures
  If a closure is the last parameter of a function, and if the closure is long, we can write it as a **trailing closure** instead. **A trailing closure is a closure that is a parameter to a function that is written after the function call's closing parenthesis.** All other parameters need to be within the function's parenthesis as normal.
- 
+
  Here's our `add` function being called using a trailing closure as a parameter:
  */
 let eric = Guest(name: "Eric", age: 19)
@@ -61,7 +53,7 @@ add(newGuest: eric) { (guestNumber: Int) in
 /*:
  ## Inferring Data Types
  In the parameter list of our closure, we can **infer** the type of each argument. This means we only need to define the names of each parameter and not include each type.
- 
+
  In our `add` funnction, we can remove the type inferrence for our `guestNumber` parameter:
  */
 
@@ -73,9 +65,9 @@ add(newGuest: eric) { guestNumber in
 /*:
  ## Shorthand Names
  We can remove the names of each parameter of the closure in favor of **shorthand names,** that will be used throughout the body of the closure in place of parameter names. $0 is the first parameter of the closure's parameter list, while $1 is the second parameter, and so on.
- 
+
  By doing this you can remove the parameter list from the closure, and each parameter's type will still be inferred like it was in the previous example.
- 
+
  Here's our `add` function again using shorthand names:
  */
 
@@ -86,7 +78,7 @@ add(newGuest: eric) {
 /*:
  ## Write closure in a single line of code
  The best part is now you can write the body of the closure all on the same line of code!
- 
+
  - note: If your closure returns a value, it too can be removed and inferred
  */
 
@@ -103,7 +95,7 @@ add(newGuest: eric) { print("There are \($0 - 1) guests ahead of me") }
 
 /*:
  Great work on becoming a master of closure optimizations! While these are not always needed, they can significantly clean up your code, and make it much easier to read and use in the future.
- 
+
  We have one more section to go, let's finish strong with memory management!
  */
 
@@ -138,4 +130,3 @@ add(newGuest: eric) { print("There are \($0 - 1) guests ahead of me") }
 
 
 import Foundation
-
